f5f23d048365298c11103a8df4c1cedf09804e1a
nanakura <1749094641@qq.com>
2023-01-21 14:31:08 +0800

    wip: unocss

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 import civetPlugin from 'vite-plugin-civet'
 import Pages from 'vite-plugin-pages'
+import Unocss from 'unocss/vite'
 
 export default defineConfig({
   plugins: [
@@ -11,8 +12,9 @@ export default defineConfig({
       outputExtension: 'tsx',
       outputTransformerPlugin: 'solid',
     }),
+    Unocss(),
     Pages({
-      extensions: ['jsx', 'tsx', 'civet', 'js', 'ts'],
+      extensions: ['jsx', 'tsx', 'civet'],
       exclude: ['**/components/*'],
     }) 
   ],
